e20576a158998f6f239645e12e09e1144dd1e2d5
maton369 <ysk901085@gmail.com>
2026-04-10 13:00:45 +0900

    feat: 実行計画

--- exec.sh
+++ exec.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-docker exec -i mysql-container \
-  mysql -u user -puserpass sample_db < ./db/init/03_select_all_explain.sql \
-  > output.log 2>&1
+# devContainer (db サービス = mysql-container) 内で実行する想定
+# 使い方: ./exec.sh <SQLファイルへのパス>
+# 例:    ./exec.sh db/init/03_select_all_explain.sql
+
+SQL_FILE="${1:-db/chapter01/03_select_all_explain.sql}"
+
+if [ ! -f "$SQL_FILE" ]; then
+  echo "SQLファイルが見つかりません: $SQL_FILE" >&2
+  exit 1
+fi
+
+# -h で接続先サービス名を指定、--protocol=tcp で強制的にネットワーク経由にする
+mysql -h mysql-container -u user -puserpass --protocol=tcp sample_db < "$SQL_FILE" > output.log 2>&1
