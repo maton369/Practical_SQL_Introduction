b5da7c6770ad41c7b6211707ec3d300aad67a278
maton369 <ysk901085@gmail.com>
2026-04-10 18:50:58 +0900

    feat: インデックススキャン

--- exec.sh
+++ exec.sh
@@ -4,7 +4,7 @@
 # 使い方: ./exec.sh <SQLファイルへのパス>
 # 例:    ./exec.sh db/init/03_select_all_explain.sql
 
-SQL_FILE="${1:-db/chapter01/03_select_all_explain.sql}"
+SQL_FILE="${1:-db/chapter01/04_explain_index_scan_shops_by_shop_id.sql}"
 
 if [ ! -f "$SQL_FILE" ]; then
   echo "SQLファイルが見つかりません: $SQL_FILE" >&2
