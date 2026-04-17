0b5f9b59144b00cc5fbead73893966eeca039a61
maton369 <ysk901085@gmail.com>
2026-04-17 13:20:31 +0900

    feat: UNION

--- exec.sh
+++ exec.sh
@@ -4,7 +4,7 @@
 # 使い方: ./exec.sh <SQLファイルへのパス>
 # 例:    ./exec.sh db/init/03_select_all_explain.sql
 
-SQL_FILE="${1:-db/chapter02/19_select_address_with_case_district.sql}"
+SQL_FILE="${1:-db/chapter02/20_select_union_address_and_address2.sql}"
 
 if [ ! -f "$SQL_FILE" ]; then
   echo "SQLファイルが見つかりません: $SQL_FILE" >&2
