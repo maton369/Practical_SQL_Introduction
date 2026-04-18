fc6f6013617ac22c1dd1889a5c51287f0d52012c
maton369 <ysk901085@gmail.com>
2026-04-18 19:20:29 +0900

    feat: INSERT

--- exec.sh
+++ exec.sh
@@ -4,7 +4,7 @@
 # 使い方: ./exec.sh <SQLファイルへのパス>
 # 例:    ./exec.sh db/init/03_select_all_explain.sql
 
-SQL_FILE="${1:-db/chapter02/25_select_dense_rank_over_order_by_age_desc.sql}"
+SQL_FILE="${1:-db/chapter02/27_insert_address_multirow.sql}"
 
 if [ ! -f "$SQL_FILE" ]; then
   echo "SQLファイルが見つかりません: $SQL_FILE" >&2
