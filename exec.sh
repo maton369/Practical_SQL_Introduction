d916d7b28408afb62d95a71d631ed56791bc9ed0
maton369 <ysk901085@gmail.com>
2026-04-22 13:42:50 +0900

    feat: クロス結合

--- exec.sh
+++ exec.sh
@@ -4,7 +4,7 @@
 # 使い方: ./exec.sh <SQLファイルへのパス>
 # 例:    ./exec.sh db/init/03_select_all_explain.sql
 
-SQL_FILE="${1:-db/chapter04/16_select_outermost_postalcode_with_nested_sets.sql}"
+SQL_FILE="${1:-db/chapter05/06_select_cross_join_employees_departments.sql}"
 
 if [ ! -f "$SQL_FILE" ]; then
   echo "SQLファイルが見つかりません: $SQL_FILE" >&2
